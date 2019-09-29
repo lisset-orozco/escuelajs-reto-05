743e7b9f837a3afb6f1cf0ea0ed8ec8b6b9cf23f
lisset-orozco <liss612@gmail.com>
2019-09-29 01:33:27 -0500

    Reto 5 completado

--- src/index.js
+++ src/index.js
@@ -1,38 +1,74 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "https://us-central1-escuelajs-api.cloudfunctions.net/characters";
+let count = 0;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
+const initialData = () => {
+  count = 0;
+  localStorage.clear();
+  intersectionObserver.observe($observe);
+};
+
+const buildElement = (tag, name, data) => {
+  let newItem = document.createElement(tag);
+  tag === "a" ? (newItem.href = name) : newItem.classList.add(name);
+  newItem.innerHTML = data;
+  $app.appendChild(newItem);
+};
+
+const endData = () => {
+  intersectionObserver.unobserve($observe);
+
+  buildElement("p", "Message", "Ya no hay más personajes...");
+  buildElement("a", "#init", "Volver al inicio");
+};
+
+const getData = async api => {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+
+    localStorage.setItem("next_fetch", data.info.next || "end");
+
+    const characters = data.results;
+    let output = characters
+      .map(character => {
         return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+            <article class="Card">
+              <img src="${character.image}" 
+              onerror="this.src='image-default.png'" alt="image"
+              />
+              <h2>(${++count}) ${character.name}
+                <span>${character.species}</span>
+              </h2>
+            </article>
+            `;
+      })
+      .join("");
+
+    buildElement("section", "Items", output);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
 
 const loadData = () => {
-  getData(API);
-}
+  let url_next = localStorage.getItem("next_fetch");
 
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+  if (url_next === "end") return endData();
+
+  getData(url_next || API);
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px"
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+);
 
-intersectionObserver.observe($observe);
+window.onload = initialData();
